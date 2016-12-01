d7d877760822c5c4151175aea583bff9e2f72ea3
Amy Roberson <amy@roberson.xyz>
2016-12-01 15:01:45 -0500

    fist commit

---
+++ nonsenseDirections.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+
+var x = "L45"
+var String = x.remove(at: x.startIndex)
+var num = Int(x)
+
---
+++ nonsenseDirections.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+enum FacingDirection {
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Turn {
+    case left
+    case right
+}
+
+
+func turnRight(direction: FacingDirection) -> FacingDirection {
+    switch direction {
+    case .north:
+        return .east
+    case .south:
+        return .west
+    case .west:
+        return .north
+    case .east:
+        return .south
+    }
+}
+
+func turnLeft(direction: FacingDirection) -> FacingDirection {
+    switch direction {
+    case .north:
+        return .west
+    case .south:
+        return .east
+    case .west:
+        return .south
+    case .east:
+        return .north
+    }
+}
+
+func goingPlaces(instructions:[(turn: Turn, distance: Int)])  {//-> String blocks away
+    var facing = FacingDirection.north
+    var x = 0 // East, West
+    var y = 0 //North, South
+    
+    for instruction in instructions {
+        
+
+        if instruction.turn == .left{
+            facing = (turnLeft(direction: facing))
+        } else {
+          facing = (turnRight(direction: facing))
+        }
+    
+        
+        
+        switch facing {
+        case .north:
+            x = x + instruction.distance
+        case .south:
+            x = x - instruction.distance
+        case .east:
+            y = y + instruction.distance
+        case .west:
+            y = y - instruction.distance
+        }
+        
+        
+    }
+    print("Block on x axis is \(x), Block on y axis is \(y)")
+    
+    
+}
+let newDirections: [(Turn, Int)] = [(.left, 45), (.right, 4)]
+
+goingPlaces(instructions: newDirections)
+
+
+let directions: [(Turn, Int)] = [(.left, 4), (.left, 4), (.left, 5), (.right, 5), (.right, 4), (.left, 4), (.right, 5), (.right, 4), (.right, 2), (.left, 3), (.right, 1), (.right, 1), (.left, 4), (.left, 5), (.right, 3), (.left, 1), (.left, 1), (.right, 4), (.left, 2), (.right, 1), (.right, 4), (.right, 4), (.left, 2), (.left, 2), (.right, 4), (.left, 4), (.right, 1), (.right, 3), (.left, 3), (.left, 1), (.left, 2), (.right, 1), (.right, 5), (.left, 5), (.left, 1), (.left, 1), (.right, 3), (.right, 5), (.left, 1), (.right, 4), (.left, 5), (.right, 5), (.right, 1), (.left, 185), (.right, 4), (.left, 1), (.right, 51), (.left, 2), (.right, 78), (.right, 1), (.left, 4), (.right, 188), (.right, 1), (.left, 5), (.right, 5), (.right, 2), (.right, 3), (.left, 5), (.right, 3), (.right, 4), (.left, 1), (.right, 2), (.right, 2), (.right, 3), (.right, 2), (.left, 5), (.right, 2), (.left, 1), (.left, 4), (.right, 4), (.left, 4), (.right, 2), (.left, 3), (.left, 4), (.right, 2), (.left, 3), (.right, 3), (.right, 2), (.left, 2), (.left, 3), (.right, 4), (.right, 3), (.right, 1), (.left, 4), (.left, 2), (.left, 5), (.right, 4), (.right, 4), (.left, 1), (.right, 1), (.left, 5), (.left, 1), (.right, 3), (.right, 1), (.left, 2), (.right, 1), (.right, 1), (.right, 3), (.left, 4), (.left, 1), (.left, 3), (.right, 2), (.right, 4), (.right, 2), (.left, 2), (.right, 1), (.left, 5), (.right, 3), (.left, 3), (.right, 3), (.left, 1), (.right, 4), (.left, 3), (.left, 3), (.right, 4), (.left, 2), (.left, 1), (.left, 3), (.right, 2), (.right, 3), (.left, 2), (.left, 1), (.right, 4), (.left, 3), (.left, 5), (.left, 2), (.left, 4), (.right, 1), (.left, 4), (.left, 4), (.right, 3), (.right, 5), (.left, 4), (.left, 1), (.left, 1), (.right, 4), (.left, 2), (.right, 5), (.right, 1), (.right, 1), (.right, 2), (.right, 1), (.right, 5), (.left, 1), (.left, 3), (.left, 5), (.right, 2), (.left, 4), (.left, 4), (.left, 5), (.right, 5), (.right, 4), (.left, 4), (.right, 5), (.right, 4), (.right, 2), (.left, 3), (.right, 1), (.right, 1), (.left, 4), (.left, 5), (.right, 3), (.left, 1), (.left, 1), (.right, 4), (.left, 2), (.right, 1), (.right, 4), (.right, 4), (.left, 2), (.left, 2), (.right, 4), (.left, 4), (.right, 1), (.right, 3), (.left, 3), (.left, 1), (.left, 2), (.right, 1), (.right, 5), (.left, 5), (.left, 1), (.left, 1), (.right, 3), (.right, 5), (.left, 1), (.right, 4), (.left, 5), (.right, 5), (.right, 1), (.left, 185), (.right, 4), (.left, 1), (.right, 51)]
+
+goingPlaces(instructions: directions)
+
